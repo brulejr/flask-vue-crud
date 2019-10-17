a03efa9012c12debcbb8b18240b8e8d733404e38
Jon Brule <brulejr@gmail.com>
2019-10-16 22:03:14 -0400

    Refactors Flask server code to separate routes and service

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,50 +1,15 @@
+from flask import jsonify, request
+from flask_cors import CORS
 import uuid
 
-from flask import Flask, jsonify, request
-from flask_cors import CORS
+from app import app
+from .books import BOOKS, remove_book
 
-
-BOOKS = [
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'On the Road',
-        'author': 'Jack Kerouac',
-        'read': True
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'Harry Potter and the Philosopher\'s Stone',
-        'author': 'J. K. Rowling',
-        'read': False
-    },
-    {
-        'id': uuid.uuid4().hex,
-        'title': 'Green Eggs and Ham',
-        'author': 'Dr. Seuss',
-        'read': True
-    }
-]
-
-# configuration
-DEBUG = True
-
-# instantiate the app
-app = Flask(__name__)
-app.config.from_object(__name__)
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-def remove_book(book_id):
-    for book in BOOKS:
-        if book['id'] == book_id:
-            BOOKS.remove(book)
-            return True
-    return False
-
-
-# sanity check route
 @app.route('/ping', methods=['GET'])
 def ping_pong():
     return jsonify('pong!')
@@ -84,7 +49,3 @@ def single_book(book_id):
         remove_book(book_id)
         response_object['message'] = 'Book removed!'
     return jsonify(response_object)
-
-
-if __name__ == '__main__':
-    app.run()
